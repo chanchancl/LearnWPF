f11242e1fc9f23f9adf7c697dd6398ef31abdae4
Ccl <ccl.love.miku@gmail.com>
2016-09-25 20:47:07 +0800

    init
    new repository for learing WPF

---
+++ WPF1/3.RoutedEvent.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// 3.RoutedEvent.xaml 的交互逻辑
+    /// </summary>
+    public partial class RoutedEvent : Window
+    {
+        public RoutedEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            lstMessage.Items.Clear();
+            eventCount = 0;
+        }
+
+        protected int eventCount = 0;
+        private void SomethingClicked(object sender, MouseButtonEventArgs e)
+        {
+            eventCount++;
+            string message = "#" + eventCount.ToString() + ":\r\n" +
+                " Sender: " + sender.ToString() + "\r\n" +
+                " Source: " + e.Source + "\r\n" +
+                " Original Source: " + e.OriginalSource;
+            lstMessage.Items.Add(message);
+            e.Handled = (bool)chkHandle.IsChecked;
+        }
+    }
+}
