0bed0d82a50eacb20f29684387d854802bdc43d9
Ccl <ccl.love.miku@gmail.com>
2016-09-26 19:51:47 +0800

    add MouseEvent

---
+++ WPF1/4.MouseEvent.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// _4.xaml 的交互逻辑
+    /// </summary>
+    public partial class MouseEvent : Window
+    {
+        public MouseEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(this);
+            tbText.Text = string.Format("You are at ({0},{1}) in window coordinates.", pt.X, pt.Y);
+        }
+    }   
+}
